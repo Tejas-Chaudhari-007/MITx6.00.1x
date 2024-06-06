834fd4608de0c90b22669c502fd923a7890b2b60
= <=>
2024-06-06 11:20:03 +0530

    Added unit 2, lecture 3 exercise: guess my number solution.

--- Unit 2/exercise - guess my number.py
+++ Unit 2/exercise - guess my number.py
@@ -1,23 +1,23 @@
 print('Please think of a number between 0 and 100!')
 low = 0
 high = 100
-guess = int((low + high) / 2.0)
-guessed_correctly = False
-while not guessed_correctly:
+guess = int((low + high) / 2)
+
+user_inp = ''
+
+while user_inp != 'c':
     print('Is your secret number ' + str(guess) + '?')
-    user_reply = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly. ")
-    if user_reply.lower() == 'c':
-        guessed_correctly = True
-        break
-    elif user_reply.lower() == 'h':
+    user_inp = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+
+    if user_inp == 'h':
         high = guess
-    elif user_reply.lower() == 'l':
+    elif user_inp == 'l':
         low = guess
+    elif user_inp == 'c':
+        print('Game over. Your secret number was:', guess)
+        break
     else:
         print('Sorry, I did not understand your input.')
         continue
-    guess = int((low + high) / 2.0)
-print('Game over. Your secret number was: ', guess)
 
-
-
+    guess = int((low + high) / 2)
